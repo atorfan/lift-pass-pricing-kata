ad7439fdab7b4e50c29c9c729763749363f6d353
alejandro.torres <alejandro.torres@holaluz.com>
2024-04-09 11:50:01 +0200

    Cost calculator converted to Kotlin

--- src/main/kotlin/dojo/liftpasspricing/domain/CostCalculator.kt
+++ src/main/kotlin/dojo/liftpasspricing/domain/CostCalculator.kt
@@ -1,80 +1,73 @@
-package dojo.liftpasspricing.domain;
+package dojo.liftpasspricing.domain
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.util.List;
+import java.time.DayOfWeek
+import java.time.LocalDate
+import kotlin.math.ceil
 
-public class CostCalculator {
+class CostCalculator(private val basePrice: Int, private val holidays: List<LocalDate>) {
 
-    private final int basePrice;
-    private final List<LocalDate> holidays;
-
-    public CostCalculator(int basePrice, List<LocalDate> holidays) {
-        this.basePrice = basePrice;
-        this.holidays = holidays;
-    }
-
-    public int calculateFor(
-            String forfaitType,
-            Integer age,
-            LocalDate priceDateRequested
-    ) {
-        int calculatedCost;
-        int reduction;
+    fun calculateFor(
+        forfaitType: String,
+        age: Int?,
+        priceDateRequested: LocalDate?
+    ): Int {
+        val calculatedCost: Int
+        var reduction: Int
 
         if (age != null && age < 6) {
-            calculatedCost = 0;
+            calculatedCost = 0
         } else {
-            reduction = 0;
+            reduction = 0
 
-            if (!forfaitType.equals("night")) {
-                boolean isHoliday = false;
+            if (forfaitType != "night") {
+                var isHoliday = false
 
-                for (LocalDate holiday : holidays) {
+                for (holiday in holidays) {
                     if (priceDateRequested != null) {
-                        if (priceDateRequested.getYear() == holiday.getYear() && //
-                                priceDateRequested.getMonth() == holiday.getMonth() && //
-                                priceDateRequested.getDayOfMonth() == holiday.getDayOfMonth()) {
-                            isHoliday = true;
+                        if (priceDateRequested.year == holiday.year
+                            && priceDateRequested.month == holiday.month
+                            && priceDateRequested.dayOfMonth == holiday.dayOfMonth
+                        ) {
+                            isHoliday = true
                         }
                     }
                 }
 
                 if (priceDateRequested != null) {
-                    if (!isHoliday && priceDateRequested.getDayOfWeek() == DayOfWeek.MONDAY) {
-                        reduction = 35;
+                    if (!isHoliday && priceDateRequested.dayOfWeek == DayOfWeek.MONDAY) {
+                        reduction = 35
                     }
                 }
 
                 // TODO apply reduction for others
                 if (age != null && age < 15) {
-                    calculatedCost = (int) Math.ceil(basePrice * .7);
+                    calculatedCost = ceil(basePrice * .7).toInt()
                 } else {
                     if (age == null) {
-                        double cost = basePrice * (1 - reduction / 100.0);
-                        calculatedCost = (int) Math.ceil(cost);
+                        val cost = basePrice * (1 - reduction / 100.0)
+                        calculatedCost = ceil(cost).toInt()
                     } else {
                         if (age > 64) {
-                            double cost = basePrice * .75 * (1 - reduction / 100.0);
-                            calculatedCost = (int) Math.ceil(cost);
+                            val cost = basePrice * .75 * (1 - reduction / 100.0)
+                            calculatedCost = ceil(cost).toInt()
                         } else {
-                            double cost = basePrice * (1 - reduction / 100.0);
-                            calculatedCost = (int) Math.ceil(cost);
+                            val cost = basePrice * (1 - reduction / 100.0)
+                            calculatedCost = ceil(cost).toInt()
                         }
                     }
                 }
             } else {
-                if (age != null && age >= 6) {
+                calculatedCost = if (age != null && age >= 6) {
                     if (age > 64) {
-                        calculatedCost = (int) Math.ceil(basePrice * .4);
+                        ceil(basePrice * .4).toInt()
                     } else {
-                        calculatedCost = basePrice;
+                        basePrice
                     }
                 } else {
-                    calculatedCost = 0;
+                    0
                 }
             }
         }
-        return calculatedCost;
+        return calculatedCost
     }
 }
